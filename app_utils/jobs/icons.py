f6398fed186db90b356170f8fcef73c360576dfa
Julien Brayere <julien.brayere@obitrain.com>
2022-11-19 14:35:57 +0100

    fix: avoid error when not using resize-icons

--- app_utils/jobs/icons.py
+++ app_utils/jobs/icons.py
@@ -1,8 +1,7 @@
 import logging
 from collections import namedtuple
 from pathlib import Path
-
-import cairosvg
+from app_utils.logs import logger
 from piou import Option
 
 Sizes = namedtuple('Sizes', ['size', 'formats'])
@@ -18,6 +17,9 @@ def run_icons(
         icon: Path | None = Option(None, '-f', '--file', help='Svg icon to convert'),
         output_folder=Option(..., '-o', '--output', help='Folder where to write the output')
 ):
+    # Import here to avoid error
+    from cairosvg import svg2png
+
     files = []
     if icons_folder:
         files += list(icons_folder.glob('*.svg'))
@@ -35,7 +37,7 @@ def run_icons(
                 _output_file = file.name.replace(file.suffix, '') + _file_ext + '.png'
                 output_size = _default_size.size * _format
                 print(f'\tsize: {output_size:<4} | ext: {_file_ext}')
-                cairosvg.svg2png(url=str(file),
-                                 output_height=output_size,
-                                 output_width=output_size,
-                                 write_to=str(output_folder / _output_file))
+                svg2png(url=str(file),
+                        output_height=output_size,
+                        output_width=output_size,
+                        write_to=str(output_folder / _output_file))
